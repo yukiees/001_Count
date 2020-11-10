bd2d4466f85dcd75a0687a1691ed76fcd96c5fe1
yukiees <yukiii8989@gmail.com>
2020-11-11 01:48:49 +0900

    Counter

--- 001_Count/ViewController.swift
+++ 001_Count/ViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func plus(){
+        number = number + 1
+        label.text = String(number)
+        color()
+    }
 
+    @IBAction func minus(_ sender: Any) {
+        number = number - 1
+        label.text = String(number)
+        color()
+    }
+    
+    @IBAction func clear(_ sender: Any) {
+        number = 0
+        label.text = String(number)
+        color()
+    }
+    
+//    func color(){
+//        if number >= 10{
+//            label.textColor = UIColor.red
+//        }else if number <= -10{
+//            label.textColor = UIColor.blue
+//        }else{
+//            label.textColor = UIColor.black
+//        }
+//    }
+ 
+    func color(){
+        switch number{
+        case Int.min ..< -10:
+            label.textColor = UIColor.blue
+            
+        case 10 ..< Int.max:
+            label.textColor = UIColor.red
+            
+        default:
+            label.textColor = UIColor.black
+        }
+    }
 }
 
