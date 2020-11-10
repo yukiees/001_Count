2f2c3c58c942fe6de3f9413e98c4a87a056acda6
yukiees <yukiii8989@gmail.com>
2020-11-11 00:58:05 +0900

    Initial Commit

---
+++ 001_CountTests/_01_CountTests.swift
@@ -0,0 +1,33 @@
+//
+//  _01_CountTests.swift
+//  001_CountTests
+//
+//  Created by 松島優希 on 2020/11/11.
+//
+
+import XCTest
+@testable import _01_Count
+
+class _01_CountTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
